440046b0647ac66c76d38e63997ae1b6cf002dc0
Dušan Reljić <reljicd@gmail.com>
2018-01-01 12:51:43 +0100

    implemented guice Module, binding for services

--- app/Module.java
+++ app/Module.java
@@ -1,15 +1,16 @@
 import com.google.inject.AbstractModule;
-import java.time.Clock;
-
-import services.ApplicationTimer;
-import services.AtomicCounter;
-import services.Counter;
+import services.CommentService;
+import services.PostService;
+import services.UserService;
+import services.impl.CommentServiceImpl;
+import services.impl.PostServiceImpl;
+import services.impl.UserServiceImpl;
 
 /**
  * This class is a Guice module that tells Guice how to bind several
  * different types. This Guice module is created when the Play
  * application starts.
- *
+ * <p>
  * Play will automatically use any class called `Module` that is in
  * the root package. You can create modules in other locations by
  * adding `play.modules.enabled` settings to the `application.conf`
@@ -19,13 +20,9 @@ public class Module extends AbstractModule {
 
     @Override
     public void configure() {
-        // Use the system clock as the default implementation of Clock
-        bind(Clock.class).toInstance(Clock.systemDefaultZone());
-        // Ask Guice to create an instance of ApplicationTimer when the
-        // application starts.
-        bind(ApplicationTimer.class).asEagerSingleton();
-        // Set AtomicCounter as the implementation for Counter.
-        bind(Counter.class).to(AtomicCounter.class);
+        bind(CommentService.class).to(CommentServiceImpl.class);
+        bind(PostService.class).to(PostServiceImpl.class);
+        bind(UserService.class).to(UserServiceImpl.class);
     }
 
 }
