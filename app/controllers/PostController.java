0d492221c5203c90d4ade3ed060dfe44f32085ae
Dušan Reljić <reljicd@gmail.com>
2018-01-03 11:59:29 +0100

    refactored PostController to use PostExistsAndUserIsOwnerAction

--- app/controllers/PostController.java
+++ app/controllers/PostController.java
@@ -1,7 +1,7 @@
 package controllers;
 
 import annotations.Authenticated;
-import dto.LoginDTO;
+import annotations.PostExistsAndUserIsOwner;
 import dto.PostDTO;
 import play.data.Form;
 import play.data.FormFactory;
@@ -15,21 +15,18 @@ import views.html.newPost;
 import views.html.post;
 
 import javax.inject.Inject;
-import java.util.Optional;
 
 public class PostController extends Controller {
 
     private final PostService postService;
     private final CommentService commentService;
     private final Form<PostDTO> postForm;
-    private final Form<LoginDTO> loginDTOForm;
 
     @Inject
     public PostController(PostService postService, CommentService commentService, FormFactory formFactory) {
         this.postService = postService;
         this.commentService = commentService;
         this.postForm = formFactory.form(PostDTO.class);
-        this.loginDTOForm = formFactory.form(LoginDTO.class);
     }
 
     public Result getPost(Long postId) {
@@ -47,11 +44,8 @@ public class PostController extends Controller {
     }
 
     @Authenticated
-//    @PostExistsAndUserIsOwner
+    @PostExistsAndUserIsOwner
     public Result getEditPostForm(Long postId) {
-        Optional<PostDTO> optionalPost = postService.getPost(postId);
-        if (optionalPost.isPresent() && !optionalPost.get().username.equals(session("username")))
-            return badRequest(views.html.login.render(loginDTOForm.withGlobalError("Please login with proper credentials to modify this post")));
         return postService.getPost(postId)
                 .map(postDTO -> ok(editPost.render(postForm.fill(postDTO), postId)))
                 .orElseGet(Results::notFound);
@@ -71,11 +65,8 @@ public class PostController extends Controller {
     }
 
     @Authenticated
-//    @PostExistsAndUserIsOwner
+    @PostExistsAndUserIsOwner
     public Result editPost(Long postId) {
-        Optional<PostDTO> optionalPost = postService.getPost(postId);
-        if (optionalPost.isPresent() && !optionalPost.get().username.equals(session("username")))
-            return badRequest(views.html.login.render(loginDTOForm.withGlobalError("Please login with proper credentials to modify this post")));
         Form<PostDTO> postForm = this.postForm.bindFromRequest();
         if (postForm.hasErrors()) {
             return badRequest(editPost.render(postForm, postId));
@@ -89,11 +80,8 @@ public class PostController extends Controller {
     }
 
     @Authenticated
-//    @PostExistsAndUserIsOwner
+    @PostExistsAndUserIsOwner
     public Result deletePost(Long postId) {
-        Optional<PostDTO> optionalPost = postService.getPost(postId);
-        if (optionalPost.isPresent() && !optionalPost.get().username.equals(session("username")))
-            return badRequest(views.html.login.render(loginDTOForm.withGlobalError("Please login with proper credentials to modify this post")));
         postService.delete(postId);
         return redirect(routes.BlogController.usersBlog(session("username"), 1));
     }
